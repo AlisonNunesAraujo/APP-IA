5b53688a6f2411c13fa27dba27a3285911f18cc0
Alison <araujoalison304@gmail.com>
2025-08-12 11:11:48 -0300

    ajustes-em-controlar-routs

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
-import { NavigationContainer } from "@react-navigation/native";
-import Routes from "./src/routs/routSTack";
-import ControlledRoutes from "./src/routs/controledRouts";
-import { useContext } from "react";
-import AuthContext from "./src/contextApi";
-
+import { NavigationContainer } from '@react-navigation/native';
+import ControlledRoutes from './src/routs/controledRouts';
+import AuthContext from './src/contextApi';
 
 function App() {
   return (
